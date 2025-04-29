1ddc1c1c52a44d9f2cf1b2aca995e0ea16a3e92a
CodewithDeep23 <deepankarsinghvm@gmail.com>
2025-04-29 15:26:06 +0530

    Add video slice and integrate with Redux store for video management

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { confgureStore, configureStore } from '@reduxjs/toolkit';
 import { authSlice } from './Slices/authSlice';
 import { userSlice } from './Slices/userSlice';
 import { darkModeSlice } from './Slices/themeSlice';
+import { videoSlice } from './Slices/videoSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authSlice,
         user: userSlice,
         darkMode: darkModeSlice,
+        video: videoSlice
     }
 })
