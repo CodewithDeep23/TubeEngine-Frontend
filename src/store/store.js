adabf0d8ac293de3581debbd89a8b862988fc3ef
CodewithDeep23 <deepankarsinghvm@gmail.com>
2025-04-30 23:09:12 +0530

    fix: Correct import statements in store.js for proper functionality

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,15 @@
-import { confgureStore, configureStore } from '@reduxjs/toolkit';
-import { authSlice } from './Slices/authSlice';
-import { userSlice } from './Slices/userSlice';
-import { darkModeSlice } from './Slices/themeSlice';
-import { videoSlice } from './Slices/videoSlice';
-import { subscriptionSlice } from './Slices/subscription';
-import { likeSlice } from './Slices/likeSlice';
-import { commentSlice } from './Slices/commentSlice';
-import { playlistSlice } from './Slices/playlistSlice';
-import { tweetSlice } from './Slices/tweetSlice';
-import { dashboardSlice } from './Slices/dashboardSlice';
-import { healthCheckSlice } from './Slices/healthcheckSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './Slices/authSlice';
+import userSlice from './Slices/userSlice';
+import darkModeSlice from './Slices/themeSlice';
+import videoSlice from './Slices/videoSlice';
+import subscriptionSlice from './Slices/subscription';
+import likeSlice from './Slices/likeSlice';
+import commentSlice from './Slices/commentSlice';
+import playlistSlice from './Slices/playlistSlice';
+import tweetSlice from './Slices/tweetSlice';
+import dashboardSlice from './Slices/dashboardSlice';
+import healthCheckSlice from './Slices/healthcheckSlice';
 
 export const store = configureStore({
     reducer: {
